d389bbd69348556df2edac0968a5e9f5720cf4a5
Nora Liu <noraliu510@gmail.com>
2025-06-13 13:50:54 -0400

    Adding timer before starting next testcase

--- src/code-runner-wc.js
+++ src/code-runner-wc.js
@@ -623,11 +623,9 @@ async function runTestCases(html_element, inputTestcase) {
 			allOutputs.push(output);
 
 			// 1 second pause
-			// await new Promise(resolve => setTimeout(resolve, 1000));
+			await new Promise(resolve => setTimeout(resolve, 1000));
 		}
 	} catch (error) {
-		// html_element.querySelector('#result').innerText = error.message;
-		// return [`Error: ${error.message}`];
 		let errrorMessage = "Please try again";
 		allOutputs.push(errrorMessage);
 	}
