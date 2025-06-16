c9c8335284043827c40346ec23e81bf2a3087572
Nora Liu <noraliu510@gmail.com>
2025-06-16 15:34:39 -0400

    Move delay to beginning of loop (fr this time)

--- src/code-runner-wc.js
+++ src/code-runner-wc.js
@@ -611,13 +611,13 @@ async function runTestCases(html_element, inputTestcase, messageElement) {
 		}
 
 		for (let i = 0; i < inputTestcase.length; i++) {
+			// X second(s) pause			
+			await new Promise(resolve => setTimeout(resolve, pause));
 
 			progressText.innerText = `Running testcase ${i + 1} of ${inputTestcase.length}...`;
 			progressText.style.color = "grey";
 			progressText.style.fontWeight = "bold";
 
-			// X second(s) pause			
-			await new Promise(resolve => setTimeout(resolve, pause));
 
 			const percent = ((i + 1) / inputTestcase.length) * 100;
 			progressBar.style.width = percent + "%";
